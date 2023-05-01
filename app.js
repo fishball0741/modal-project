c32105c7539e30b006e2f823bfe34de9d1cbbd4a
fishball0741 <fishball0741@gmail.com>
2023-05-01 15:31:32 +1000

    modal completed

---
+++ app.js
@@ -0,0 +1,21 @@
+// select modal-btn,modal-overlay,close-btn
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+modalBtn.addEventListener('click', function() {
+    modalOverlay.classList.add('open-modal')
+})
+
+// listen for click events on modal-btn and close-btn
+// when user clicks close-btn remove .open-modal from modal-overlay
+closeBtn.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal')
+
+})
+
+
+// make sure the CSS are completed  before this code, because it must hidden the content first.
+// see styles.css, modal-overlay, visibility and z-index.
